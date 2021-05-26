d118734f2cee1df64df02e0421fff68aa5c51cc7
Krishna Vamsi <46665678+dnskvamsi@users.noreply.github.com>
2021-05-26 16:29:31 +0530

    Add files via upload

---
+++ HTML Parser - Part 2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from html.parser import HTMLParser
+html = ""
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self,data):
+        if('\n' in data):
+            print(">>> Multi-line Comment")
+        else:
+            print(">>> Single-line Comment")
+        print(data)
+    def handle_data(self,data):
+        if(data != '\n'):
+            print(">>> Data")
+            print(data)
+             
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
+
